b2d6be913c30b9c6703eeeedb6a4c847b27bb9ab
vishnu-pc <vishnupc1999@gmail.com>
2022-03-16 00:55:59 +0530

    Logo Updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
-import logo from '../assets/images/paradise-black.jpeg';
+import logo from '../assets/images/paradise-transparent.png';
 import './Header.css';
 
 function Header() {
