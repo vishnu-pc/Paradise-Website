6d2cfd8aaf0b272c95ecfd852284090b45843663
Sidharth <sidharth@eltropy.com>
2022-03-18 20:22:23 +0530

    commit 1

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,15 +1,19 @@
-import React from 'react'
-import './Test.css'
+import React from "react";
+import "./Test.css";
 
 function Test() {
-    return (
-        <div class="test1">
-            <div class="test2">
-                <h1>Paradise</h1>
-                <p>Style Studio</p>
-            </div>
-        </div>
-    );
+  return (
+    <div class="test1">
+      <div class="test2">
+        <h1>Paradise</h1>
+        <p>Style Studio</p>
+      </div>
+      <div class="test2">
+        <h1>Paradise</h1>
+        <p>Style Studio</p>
+      </div>
+    </div>
+  );
 }
 
-export default Test
+export default Test;
