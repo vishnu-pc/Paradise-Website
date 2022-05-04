55f756e6bbef364aeb182b1281ec9e07fdf4e96a
vishnu-pc <vishnupc1999@gmail.com>
2022-05-04 23:58:02 +0530

    Logo url linked

--- src/components/Header2.js
+++ src/components/Header2.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
-import logo from "../assets/images/paradise-transparent.png";
 import "./Header.css";
 
 function Header2() {
